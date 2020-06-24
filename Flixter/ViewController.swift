3e8ed0afc9c9c5aba4c24fa0453ddcf3ac240921
Pooja Yadav <poyadav@ucsd.edu>
2020-06-23 18:17:04 -0700

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Pooja Yadav on 6/23/20.
+//  Copyright © 2020 Pooja Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
