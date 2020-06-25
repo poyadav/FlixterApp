efea78968ede32dc549ed96aa9c5302857a0e185
Pooja Yadav <poyadav@ucsd.edu>
2020-06-25 01:02:35 -0700

    Added a collection view and tab bar controller.

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Pooja Yadav on 6/24/20.
+//  Copyright © 2020 Pooja Yadav. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
